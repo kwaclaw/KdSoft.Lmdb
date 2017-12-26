9e3a7c7f44d618b73f13db67f1431d0635239994
Karl Waclawek <karl@waclawek.net>
2017-12-26 12:32:35 -0500

    Moved DbValue to interop folder - not meant to be used by clients.

---
+++ Lmdb/interop/DbValue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace KdSoft.Lmdb
+{
+    [CLSCompliant(false)]
+    [StructLayout(LayoutKind.Sequential, Pack = Compile.PackSize)]
+    public unsafe struct DbValue
+    {
+        public DbValue(void* data, long size) {
+            this.Size = (IntPtr)size;
+            this.Data = data;
+        }
+        public DbValue(void* data, int size) {
+            this.Size = (IntPtr)size;
+            this.Data = data;
+        }
+
+        public readonly IntPtr Size;
+        public readonly void* Data;
+
+        public ReadOnlySpan<byte> ToReadOnlySpan() {
+            return new ReadOnlySpan<byte>(Data, unchecked((int)Size));
+        }
+
+        public Span<byte> ToSpan() {
+            return new Span<byte>(Data, unchecked((int)Size));
+        }
+    }
+}
