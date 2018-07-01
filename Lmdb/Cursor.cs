1cfe03e0592aa0c9be36cc4ecd2214068182d344
Karl Waclawek <karl@waclawek.net>
2018-07-01 19:50:18 -0400

    More XML doc comments for Cursor.cs.

--- Lmdb/Cursor.cs
+++ Lmdb/Cursor.cs
@@ -24,6 +24,7 @@ namespace KdSoft.Lmdb
             set => disposed = value;
         }
 
+        /// <summary>Native transaction handle. </summary>
         public IntPtr TxnHandle {
             get {
                 lock (rscLock) {
@@ -33,8 +34,10 @@ namespace KdSoft.Lmdb
             }
         }
 
+        /// <summary>Indicates if cursor is read-only. </summary>
         public bool IsReadOnly => isReadOnly;
 
+        /// <summary>Native database handle. </summary>
         public IntPtr DbiHandle {
             get {
                 lock (rscLock) {
@@ -53,6 +56,10 @@ namespace KdSoft.Lmdb
             Dispose();
         }
 
+        /// <summary>
+        /// Renews cursor, allows re-use without re-allocation.
+        /// Only valid for read-only cursors.
+        /// </summary>
         public void Renew(ReadOnlyTransaction transaction) {
             if (!IsReadOnly)
                 throw new InvalidOperationException("Only read-only cursors can be renewed.");
@@ -66,7 +73,11 @@ namespace KdSoft.Lmdb
 
         #region GET implementations
 
-        // valid operations: MDB_SET_KEY, MDB_SET_RANGE,
+        /// <summary>
+        /// Internal GET operation. Valid for <see cref="DbCursorOp.MDB_SET_KEY"/> and <see cref="DbCursorOp.MDB_SET_RANGE"/>.
+        /// </summary>
+        /// <returns><c>true</c>if record was found, <c>false</c> otherwise.</returns>
+        /// <exception cref="LmdbException">Exception based on native return code.</exception>
         protected bool Get(in ReadOnlySpan<byte> key, out KeyDataPair entry, DbCursorOp op) {
             DbRetCode ret;
             lock (rscLock) {
@@ -86,7 +97,11 @@ namespace KdSoft.Lmdb
             return true;
         }
 
-        // valid operations: MDB_GET_BOTH, MDB_GET_BOTH_RANGE
+        /// <summary>
+        /// Internal GET operation. Valid for <see cref="DbCursorOp.MDB_GET_BOTH"/> and <see cref="DbCursorOp.MDB_GET_BOTH_RANGE"/>.
+        /// </summary>
+        /// <returns><c>true</c>if record was found, <c>false</c> otherwise.</returns>
+        /// <exception cref="LmdbException">Exception based on native return code.</exception>
         protected bool Get(in KeyDataPair keyData, out KeyDataPair entry, DbCursorOp op) {
             DbRetCode ret;
             lock (rscLock) {
@@ -107,7 +122,12 @@ namespace KdSoft.Lmdb
             return true;
         }
 
-        // valid operations: MDB_SET, does not return key or data, 
+        /// <summary>
+        /// Internal MOVE TO KEY operation. Valid for <see cref="DbCursorOp.MDB_SET"/>.
+        /// Does not return key or data.
+        /// </summary>
+        /// <returns><c>true</c>if record was found, <c>false</c> otherwise.</returns>
+        /// <exception cref="LmdbException">Exception based on native return code.</exception>
         protected bool MoveToKey(in ReadOnlySpan<byte> key, DbCursorOp op) {
             DbRetCode ret;
             lock (rscLock) {
@@ -125,7 +145,13 @@ namespace KdSoft.Lmdb
             return true;
         }
 
-        // valid operations: MDB_GET_CURRENT, MDB_FIRST, MDB_NEXT, MDB_PREV, MDB_LAST
+        /// <summary>
+        /// Internal GET KEY operation. Valid for <see cref="DbCursorOp.MDB_GET_CURRENT"/>,
+        /// <see cref="DbCursorOp.MDB_FIRST"/>, <see cref="DbCursorOp.MDB_NEXT"/>, <see cref="DbCursorOp.MDB_PREV"/>
+        /// and <see cref="DbCursorOp.MDB_LAST"/>. Returns key, but not data.
+        /// </summary>
+        /// <returns><c>true</c>if record was found, <c>false</c> otherwise.</returns>
+        /// <exception cref="LmdbException">Exception based on native return code.</exception>
         protected bool GetKey(out ReadOnlySpan<byte> key, DbCursorOp op) {
             DbRetCode ret;
             lock (rscLock) {
@@ -142,7 +168,13 @@ namespace KdSoft.Lmdb
             return true;
         }
 
-        // valid operations: MDB_GET_CURRENT, MDB_FIRST, MDB_NEXT, MDB_PREV, MDB_LAST
+        /// <summary>
+        /// Internal GET operation. Valid for <see cref="DbCursorOp.MDB_GET_CURRENT"/>,
+        /// <see cref="DbCursorOp.MDB_FIRST"/>, <see cref="DbCursorOp.MDB_NEXT"/>, <see cref="DbCursorOp.MDB_PREV"/>
+        /// and <see cref="DbCursorOp.MDB_LAST"/>. Returns both, key and data.
+        /// </summary>
+        /// <returns><c>true</c>if record was found, <c>false</c> otherwise.</returns>
+        /// <exception cref="LmdbException">Exception based on native return code.</exception>
         protected bool Get(out KeyDataPair entry, DbCursorOp op) {
             DbRetCode ret;
             lock (rscLock) {
@@ -160,10 +192,14 @@ namespace KdSoft.Lmdb
             return true;
         }
 
-        // valid operations: MDB_FIRST_DUP, MDB_NEXT_DUP, MDB_NEXT_NODUP,
-        //                   MDB_PREV_DUP, MDB_PREV_NODUP, MDB_LAST_DUP,
-        //                   MDB_GET_MULTIPLE, MDB_NEXT_MULTIPLE, MDB_PREV_MULTIPLE
-        // returns data, but not key
+        /// <summary>
+        /// Internal GET DATA operation. Valid for <see cref="DbCursorOp.MDB_FIRST_DUP"/>, <see cref="DbCursorOp.MDB_NEXT_DUP"/>, 
+        /// <see cref="DbCursorOp.MDB_NEXT_NODUP"/>, <see cref="DbCursorOp.MDB_PREV_DUP"/>, <see cref="DbCursorOp.MDB_PREV_NODUP"/>
+        /// <see cref="DbCursorOp.MDB_LAST_DUP"/>, <see cref="DbCursorOp.MDB_GET_MULTIPLE"/>, <see cref="DbCursorOp.MDB_NEXT_MULTIPLE"/>
+        /// and <see cref="DbCursorOp.MDB_PREV_MULTIPLE"/>. Returns data, but not key.
+        /// </summary>
+        /// <returns><c>true</c>if record was found, <c>false</c> otherwise.</returns>
+        /// <exception cref="LmdbException">Exception based on native return code.</exception>
         protected bool GetData(out ReadOnlySpan<byte> data, DbCursorOp op) {
             DbRetCode ret;
             lock (rscLock) {
@@ -189,7 +225,7 @@ namespace KdSoft.Lmdb
         /// Moves cursor to key position.
         /// </summary>
         /// <param name="key"></param>
-        /// <returns><c>true</c> if key exists, false otherwise.</returns>
+        /// <returns><c>true</c> if successful (key exists), false otherwise.</returns>
         public bool MoveToKey(in ReadOnlySpan<byte> key) {
             return MoveToKey(in key, DbCursorOp.MDB_SET);
         }
@@ -199,7 +235,7 @@ namespace KdSoft.Lmdb
         /// </summary>
         /// <param name="key"></param>
         /// <param name="entry"></param>
-        /// <returns><c>true</c> if key exists, false otherwise.</returns>
+        /// <returns><c>true</c> if successful (key exists), false otherwise.</returns>
         public bool GetAt(in ReadOnlySpan<byte> key, out KeyDataPair entry) {
             return Get(in key, out entry, DbCursorOp.MDB_SET_KEY);
         }
@@ -209,7 +245,7 @@ namespace KdSoft.Lmdb
         /// </summary>
         /// <param name="key"></param>
         /// <param name="entry"></param>
-        /// <returns><c>true</c> if nearest key exists, false otherwise.</returns>
+        /// <returns><c>true</c> if successful (nearest key exists), false otherwise.</returns>
         public bool GetNearest(in ReadOnlySpan<byte> key, out KeyDataPair entry) {
             return Get(in key, out entry, DbCursorOp.MDB_SET_RANGE);
         }
@@ -218,7 +254,7 @@ namespace KdSoft.Lmdb
         /// Gets key at current position.
         /// </summary>
         /// <param name="key"></param>
-        /// <returns><c>true</c> if key exists, false otherwise.</returns>
+        /// <returns><c>true</c> if successful (key exists), false otherwise.</returns>
         public bool GetCurrent(out ReadOnlySpan<byte> key) {
             return GetKey(out key, DbCursorOp.MDB_GET_CURRENT);
         }
@@ -227,7 +263,7 @@ namespace KdSoft.Lmdb
         /// Get record (key and data) at current position.
         /// </summary>
         /// <param name="entry"></param>
-        /// <returns><c>true</c> if key exists, false otherwise.</returns>
+        /// <returns><c>true</c> if successful (key exists), false otherwise.</returns>
         public bool GetCurrent(out KeyDataPair entry) {
             return Get(out entry, DbCursorOp.MDB_GET_CURRENT);
         }
@@ -236,7 +272,7 @@ namespace KdSoft.Lmdb
         /// Move cursor to next position and get the record (key and data).
         /// </summary>
         /// <param name="entry"></param>
-        /// <returns><c>true</c> if next key exists, false otherwise.</returns>
+        /// <returns><c>true</c> if successful (next key exists), false otherwise.</returns>
         public bool GetNext(out KeyDataPair entry) {
             return Get(out entry, DbCursorOp.MDB_NEXT);
         }
@@ -245,7 +281,7 @@ namespace KdSoft.Lmdb
         /// Move cursor to previous position and get the record (key and data).
         /// </summary>
         /// <param name="entry"></param>
-        /// <returns><c>true</c> if previous key exists, false otherwise.</returns>
+        /// <returns><c>true</c> if successful (previous key exists), false otherwise.</returns>
         public bool GetPrevious(out KeyDataPair entry) {
             return Get(out entry, DbCursorOp.MDB_PREV);
         }
@@ -254,6 +290,12 @@ namespace KdSoft.Lmdb
 
         #region Update Operations
 
+        /// <summary>
+        /// Internal PUT operation. Inserts or updates data at key position.
+        /// </summary>
+        /// <returns><c>true</c> if inserted or updated without error, <c>false</c> if
+        /// <see cref="CursorPutOption.NoOverwrite"/> was specified and the key already exists.</returns>
+        /// <exception cref="LmdbException">Exception based on native return code.</exception>
         [CLSCompliant(false)]
         protected bool PutInternal(in KeyDataPair entry, uint option) {
             DbRetCode ret;
@@ -282,8 +324,8 @@ namespace KdSoft.Lmdb
         /// </summary>
         /// <param name="entry"></param>
         /// <param name="option"></param>
-        /// <remarks><c>true</c> if inserted without error, <c>false</c> if <see cref="CursorPutOption.NoOverwrite"/>
-        /// was specified and the key already exists.</remarks>
+        /// <returns><c>true</c> if inserted or updated without error, <c>false</c> if <see cref="CursorPutOption.NoOverwrite"/>
+        /// was specified and the key already exists.</returns>
         public bool Put(in KeyDataPair entry, CursorPutOption option) {
             return PutInternal(in entry, unchecked((uint)option));
         }
@@ -386,23 +428,23 @@ namespace KdSoft.Lmdb
         #region Enumeration
 
         /// <summary>
-        /// Iterates over all records in sort order.
+        /// Iterates over all records in sort order. For use in foreach loop.
         /// </summary>
         public ItemsIterator Forward => new ItemsIterator(this, DbCursorOp.MDB_FIRST, DbCursorOp.MDB_NEXT);
 
         /// <summary>
-        /// Iterates over records in sort order, from the next position on.
+        /// Iterates over records in sort order, from the next position on. For use in foreach loops.
         /// If duplicates are allowed, then the next position may be on the same key, in duplicate sort order.
         /// </summary>
         public NextItemsIterator ForwardFrom => new NextItemsIterator(this, DbCursorOp.MDB_NEXT);
 
         /// <summary>
-        /// Iterates over all records in reverse sort order.
+        /// Iterates over all records in reverse sort order. For use in foreach loops.
         /// </summary>
         public ItemsIterator Reverse => new ItemsIterator(this, DbCursorOp.MDB_LAST, DbCursorOp.MDB_PREV);
 
         /// <summary>
-        /// Iterates over records in reverse sort order, from the previous position on.
+        /// Iterates over records in reverse sort order, from the previous position on. For use in foreach loops.
         /// If duplicates are allowed, then the previous position may be on the same key, in reverse duplicate sort order.
         /// </summary>
         public NextItemsIterator ReverseFrom => new NextItemsIterator(this, DbCursorOp.MDB_PREV);
@@ -415,6 +457,14 @@ namespace KdSoft.Lmdb
 
         public delegate bool GetItem(out KeyDataPair item);
 
+        /// <summary>
+        /// Implements the Enumerable pattern for use with foreach loops when the items to be iterated over
+        /// can only live on the stack. This works because the foreach loop does not require an explicit
+        /// implementaion of the <see cref="System.Collections.IEnumerable"/> interface.
+        /// </summary>
+        /// <remarks><a href="https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerable#remarks">
+        /// See Remarks in the <c>IEnumerable</c> documentation.
+        /// </a></remarks>  
         public struct ItemsIterator
         {
             readonly Cursor cursor;
@@ -433,6 +483,14 @@ namespace KdSoft.Lmdb
             public ItemsEnumerator GetEnumerator() => new ItemsEnumerator(GetFirstItem, GetNextItem);
         }
 
+        /// <summary>
+        /// Implements the Enumerable pattern for use with foreach loops when the items to be iterated over
+        /// can only live on the stack. This works because the foreach loop does not require an explicit
+        /// implementaion of the <see cref="System.Collections.IEnumerable"/> interface.
+        /// </summary>
+        /// <remarks><a href="https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerable#remarks">
+        /// See Remarks in the <c>IEnumerable</c> documentation.
+        /// </a></remarks>  
         public ref struct NextItemsIterator
         {
             readonly Cursor cursor;
@@ -446,6 +504,14 @@ namespace KdSoft.Lmdb
             public NextItemsEnumerator GetEnumerator() => new NextItemsEnumerator(cursor, nextOp);
         }
 
+        /// <summary>
+        /// Implements Enumerator pattern for use with foreach loops when the items to be iterated over
+        /// can only live on the stack. This works because the foreach loop does not require an explicit
+        /// implementaion of the <see cref="System.Collections.IEnumerator"/> interface.
+        /// </summary>
+        /// <remarks><a href="https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerable#remarks">
+        /// See Remarks in the <c>IEnumerable</c> documentation.
+        /// </a></remarks>  
         public ref struct ItemsEnumerator
         {
             readonly GetItem getFirst;
@@ -483,6 +549,14 @@ namespace KdSoft.Lmdb
             }
         }
 
+        /// <summary>
+        /// Implements Enumerator pattern for use with foreach loops when the items to be iterated over
+        /// can only live on the stack. This works because the foreach loop does not require an explicit
+        /// implementation of the <see cref="System.Collections.IEnumerator"/> interface.
+        /// </summary>
+        /// <remarks><a href="https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerable#remarks">
+        /// See Remarks in the <c>IEnumerable</c> documentation.
+        /// </a></remarks>  
         public ref struct NextItemsEnumerator
         {
             readonly Cursor cursor;
